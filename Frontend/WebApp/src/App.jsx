bbdfce0035d20d6175301a535847ace20141b440
Ajay Kumar R <r.ajaykumars@gmail.com>
2025-02-01 11:38:04 +0530

    [Frontend] Flowbite installed
    Installed Flowbite in WebApp section

--- Frontend/WebApp/src/App.jsx
+++ Frontend/WebApp/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { Button } from 'flowbite-react'
 import './App.css'
 
 function App() {
@@ -28,6 +29,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div className="h-screen flex items-center justify-center bg-gray-100">
+      <Button color="blue" onClick={() => alert('Flowbite Button Clicked!')}>
+        Click Me
+      </Button>
+    </div>
     </>
   )
 }
